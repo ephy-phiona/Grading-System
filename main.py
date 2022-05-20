60ba9b2924fffd570277fb05626b86c55febdb08
ephy-phiona <ephyphiona@gmail.com>
2022-05-20 21:55:04 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+range(1,100)
+choice = int(input("Enter your scores: "))
+# if choice >=90:
+#     print("A")    
+if choice >= 81 and 100:
+        print("A") 
+elif choice >= 74 and 80:
+        print("A-") 
+elif choice >= 67 and  73:
+        print("B+") 
+elif choice >= 60 and  66:
+         print("B") 
+elif choice >= 53 and  59:
+        print("B-") 
+elif choice >= 46 and  52:
+        print("C+") 
+elif choice >= 39 and  45:
+        print("C") 
+elif choice >= 32 and  38:
+        print("C-") 
+elif choice >= 25 and  31:    
+        print("D+") 
+elif choice >= 18 and  24:
+        print("D") 
+elif choice >= 11 and  17:
+        print("D-")  
+elif choice >= 11 and  17:
+        print("E")                                
+else:
+         print("F") 
